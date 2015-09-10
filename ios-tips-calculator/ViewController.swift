ec434affc11b05fc4280edb346ec870ce7331364
Yayang Tian <ytian@walmartlabs.com>
2015-09-09 19:05:02 -0700

    Implemented basic calculation based on input.

---
+++ ios-tips-calculator/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ios-tips-calculator
+//
+//  Created by Yayang Tian on 9/9/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        let billAmount = NSString(string: billField.text!).doubleValue;
+        let tip = billAmount * 0.2
+        let total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+}
+
