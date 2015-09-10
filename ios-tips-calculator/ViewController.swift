7e3d0912a086f1cc9d96086ed08053f3c2de4d6e
Yayang Tian <ytian@walmartlabs.com>
2015-09-09 21:13:04 -0700

    Added setting page.

--- ios-tips-calculator/ViewController.swift
+++ ios-tips-calculator/ViewController.swift
@@ -41,5 +41,26 @@ class ViewController: UIViewController {
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
+    }
+
 
 }
