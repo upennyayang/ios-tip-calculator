5557381b84a230434cc55da5b76aec85284c734d
Yayang Tian <ytian@walmartlabs.com>
2015-09-09 20:12:13 -0700

    Implemented basic functions.

--- ios-tips-calculator/ViewController.swift
+++ ios-tips-calculator/ViewController.swift
@@ -3,16 +3,18 @@
 //  ios-tips-calculator
 //
 //  Created by Yayang Tian on 9/9/15.
-//  Copyright © 2015 codepath. All rights reserved.
+//  Copyright © 2015 Walmart Labs. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var percentageControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,12 +27,19 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onEditingChanged(sender: AnyObject) {
-        let billAmount = NSString(string: billField.text!).doubleValue;
-        let tip = billAmount * 0.2
+        let percentages = [0.18, 0.20, 0.22]
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let percentage = percentages[tipControl.selectedSegmentIndex]
+        let tip = billAmount * percentage
         let total = billAmount + tip
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
-}
 
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+
+}
